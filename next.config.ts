ce9aa1696b287a244de5e3dc5f88b954d43cf685
bheki daweti <bheki.daweti@gmail.com>
2025-01-19 19:40:46 +0200

    next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  output: 'standalone', 
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+};
+
+module.exports = nextConfig;
+
